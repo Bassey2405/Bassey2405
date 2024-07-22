45bc09fd78938f4bb190493a4276f3222bcbfaf1
Bassey2405 <128388857+Bassey2405@users.noreply.github.com>
2024-07-22 20:37:49 +0100

    Add files via upload

---
+++ JavaDevApp/FirstProject.java
@@ -0,0 +1,12 @@
+//package firstproject;
+/**
+ * FirstProject
+ */
+class FirstProject {
+
+
+    public static void main(String[] args) {
+     System.out.print("Hello World, I am learning to code in Java");   
+    }
+    
+}
